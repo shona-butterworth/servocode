1f94382a48a599533ddb77f7c560af07e2105fd2
shona-butterworth <147392799+shona-butterworth@users.noreply.github.com>
2024-06-05 15:32:57 +0100

    Update servo.js

--- Teachable_Machine_1_Image_Classification_+_Physical_Output/servo.js
+++ Teachable_Machine_1_Image_Classification_+_Physical_Output/servo.js
@@ -1,20 +1,14 @@
-// Teachable Machine ml5 image example - modified from The Coding Train https://thecodingtrain.com/TeachableMachine/1-teachable-machine.html
 let video;
 let label = "waiting...";
 let confidence = 0.0;
 let classifier;
 let modelURL = 'https://teachablemachine.withgoogle.com/models/25lPyN_lh/';
-
-
-//Arduino comms variables
 let port;
 let connectBtn;
 let value = 0;
 
-// STEP 1: Load the model!
 function preload() {
   classifier = ml5.imageClassifier(modelURL + 'model.json');
-
 }
 
 function setup() {
@@ -22,12 +16,8 @@ function setup() {
   video = createCapture(VIDEO);
   video.hide();
   classifyVideo();
-
-  //Arduino setup
   port = createSerial();
 
-  // any other ports can be opened via a dialog after
-  // user interaction (see connectBtnClick below)
   connectBtn = createButton('Connect to Arduino');
   connectBtn.position(20, 20);
   connectBtn.mousePressed(connectBtnClick);
@@ -37,7 +27,6 @@ function draw() {
   background(0);
   image(video, 0, 0, width, 480);
 
-  // STEP 4: Show image + current label if confidence is over a set value
   if (label == "white" && confidence > 0.5) {
     value = 90;
     port.write(90 + '\n');
@@ -55,20 +44,18 @@ function draw() {
   fill(255);
   text(label + " " + confidence, width / 2, height - 16);
 
-  //send to Arduino
   if (frameCount % 10 == 0) {
     port.write(value + '\n'); //finish with a newline character for Arduino recieving
 
   }
 
-  // changes button label based on connection status
   if (!port.opened()) {
     connectBtn.html('Connect to Arduino');
   } else {
     connectBtn.html('Disconnect');
   }
 }
-//Arduino connect function
+
 function connectBtnClick() {
   if (!port.opened()) {
     port.open('Arduino', 115200);
@@ -77,13 +64,10 @@ function connectBtnClick() {
   }
 }
 
-
-// STEP 2: Do the classifying
 function classifyVideo() {
   classifier.classify(video, gotResults);
 }
 
-// STEP 3: Get the classification
 function gotResults(error, results) {
   if (error) {
     console.error(error);
